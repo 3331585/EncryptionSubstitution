d63d23d5bbad46108f38d8e4549d4a973c8f5a7d
3331585 <49224940+3331585@users.noreply.github.com>
2019-04-24 20:17:02 +1000

    Update SubstitutionEncryption.c

--- SubstitutionEncryption.c
+++ SubstitutionEncryption.c
@@ -2,6 +2,21 @@
 
 int main()
 {
-
+    char messageletter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char ciphertextletter[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    char input[] = "JAMIE";
+    int n = 5;
+    int index = 0;
+    int counter = 0;
+    
+    while (index <= n){
+        for (counter = 0; counter < 25; counter ++){
+            if (input[index] == messageletter[counter]){
+            printf("%c", ciphertextletter[counter]);
+            }
+        }
+        index ++;
+    }
+    
 return 0;
 }
